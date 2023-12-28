b41ad322dbf38d8853217cc42a9b3214b50b2f6f
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-12-27 21:35:48 -0300

    atualização do frontend

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-const mainSectionElement = document.getElementById("main_section__transacao");
-
-mainSectionElement.addEventListener("click", () => {
-  mainSectionElement.style.display = "none";
-});
